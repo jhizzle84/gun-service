b9b0b33695f4e987ca93c3021db0da8dd63c57ea
Daniel Lugo <danlugo92@gmail.com>
2019-08-14 18:22:49 -0400

    expose key submodule

--- src/contact-api/index.js
+++ src/contact-api/index.js
@@ -5,7 +5,8 @@ import * as Actions from "./actions.js";
 import * as Events from "./events.js";
 import * as Gun from "./gun.js";
 import * as Jobs from "./jobs.js";
+import * as Key from "./key.js";
 import * as Schema from "./schema.js";
 import * as Testing from "./testing.js";
 
-export { Actions, Events, Gun, Jobs, Schema, Testing };
+export { Actions, Events, Gun, Jobs, Key, Schema, Testing };
